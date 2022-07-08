8db2a8b5009ca4c36c91c3d874044ebf66278be5
Yeow Heng <20042741@myrp.edu.sg>
2022-07-08 11:47:05 +0800

    sharedpreference for name and gpa

--- app/src/main/java/sg/edu/rp/c346/id20042741/demomyprofile/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id20042741/demomyprofile/MainActivity.java
@@ -2,13 +2,45 @@ package sg.edu.rp.c346.id20042741.demomyprofile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etName;
+    EditText etGPA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.nameTEID);
+        etGPA = findViewById(R.id.gpaTEID);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        String savedName = prefs.getString("name", "Enter your name");
+        float savedGPA = prefs.getFloat("gpa",0);
+        etName.setText(savedName);
+        etGPA.setText(String.format("%.2f", savedGPA));
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        String strName = etName.getText().toString();
+        float gpa = Float.parseFloat(etGPA.getText().toString());
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        prefEdit.putString("name", strName);
+        prefEdit.putFloat("gpa", gpa);
+        prefEdit.commit();
     }
 }
